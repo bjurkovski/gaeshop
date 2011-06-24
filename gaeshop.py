0f65b4047a02d259c8648c1bd2ed14791dbd79ae
Bruno Jurkovski <bjurkovski@gmail.com>
2011-06-24 14:26:56 -0300

    added logo

--- gaeshop.py
+++ gaeshop.py
@@ -13,21 +13,51 @@ TEMPLATES_DIR = os.path.join(os.path.dirname(__file__), 'templates/')
 
 class Home(webapp.RequestHandler):
 	def get(self):
-			user = users.get_current_user()
+		user = users.get_current_user()
 
-			param = {'user': user,
-					 'isAdmin': users.is_current_user_admin(),
-					 'loginURL': users.create_login_url("/"),
-					 'logoutURL': users.create_logout_url("/")
-					}
+		param = {'user': user,
+				 'isAdmin': users.is_current_user_admin(),
+				 'loginURL': users.create_login_url("/"),
+				 'logoutURL': users.create_logout_url("/")
+				}
 
-			self.response.out.write(template.render(TEMPLATES_DIR + "main.html", param))
+		self.response.out.write(template.render(TEMPLATES_DIR + "main.html", param))
+
+class ViewCart(webapp.RequestHandler):
+	def get(self):
+		user = users.get_current_user()
+
+		param = {'user': user,
+				 'isAdmin': users.is_current_user_admin(),
+				 'loginURL': users.create_login_url("/"),
+				 'logoutURL': users.create_logout_url("/")
+				}
+
+		self.response.out.write(template.render(TEMPLATES_DIR + "cart.html", param))
+
+class ViewProduct(webapp.RequestHandler):
+	def get(self, productId):
+		productId = urllib.unquote(urllib.unquote(kind))
+
+class RegisterProduct(webapp.RequestHandler):
+	def get(self):
+		user = users.get_current_user()
+
+		param = {'user': user,
+				 'isAdmin': users.is_current_user_admin(),
+				 'loginURL': users.create_login_url("/"),
+				 'logoutURL': users.create_logout_url("/")
+				}
+
+		self.response.out.write(template.render(TEMPLATES_DIR + "registerProduct.html", param))
 
 application = webapp.WSGIApplication(
 									[
 									# URLs go here
 									 ('/', Home),
-									 ('/home', Home),
+									 ('/view/cart', ViewCart),
+									 ('/view/product/([^/]+)', ViewProduct),
+									 ('/register/product', RegisterProduct),
 									 ('/.*', Home)
 									],
 									debug=True)
