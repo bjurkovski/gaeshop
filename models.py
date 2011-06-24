0b72658605abaef22d6fd3f167a38db05108f959
Lucas Fialho Zawacki <lfzawacki@gmail.com>
2011-06-24 17:59:42 -0300

    fix (Dummy methods in models)

--- models.py
+++ models.py
@@ -3,8 +3,21 @@ from google.appengine.ext import db
 class Product(db.Model):
     price = db.FloatProperty()
     name = db.StringProperty()
+    description = db.StringProperty()
     stock = db.IntegerProperty()
     views = db.IntegerProperty()
+
+    def __init__(self, name, description, price, stock)
+        pass
+
+    def getStock(self):
+        return 0
+
+    def match(self, search):
+        return True
+
+    def addView(self):
+        self.view += 1
 
 class Order(db.Model):
     user = db.UserProperty()
@@ -16,7 +29,12 @@ class PaymentReceiver(db.Model):
     pass
 
 class ShoppingCart(db.Model):
-    pass
+
+    def addProduct(self,product): 
+        return True
+
+    def getProducts(self)
+        return []
 
 class User(db.Model):
     name = db.StringProperty()
@@ -26,3 +44,18 @@ class User(db.Model):
     zipcode = db.StringProperty()
     password = db.StringProperty()
     code = db.StringProperty()
+
+    def addProductToShoppingCart(self,product):
+        pass
+        
+    def getAccount(self):
+        pass
+
+    def getShoppingCart(self):
+        pass
+
+    def getOrders(self):
+        pass
+
+    def addOrder(self,paymentMethod,shippingAddress):
+        return True
